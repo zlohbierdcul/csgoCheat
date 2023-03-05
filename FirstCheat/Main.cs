7d57184c03f1ec423b50e56b93933f84a5a81548
zlohbierdcul <82916072+zlohbierdcul@users.noreply.github.com>
2023-03-05 21:56:36 +0100

    added OFFSETS and added cheatThread

---
+++ FirstCheat/Main.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+
+namespace FirstCheat
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			StartCheatThread();
+			Console.ReadKey();
+
+		}
+
+		public static nint StartInjection()
+		{
+			VAMemory vaMemory = new VAMemory("csgo");
+
+			var client = (nint)0;
+			var engine = (nint)0;
+
+			try
+			{
+				client = vaMemory.GetModuleBaseAddress("client.dll");
+				engine = vaMemory.GetModuleBaseAddress("engine.dll");
+			}
+			catch { }
+
+			return client;
+		}
+
+		public static void StartCheatThread()
+		{
+			Thread cheatThread = new Thread(new ThreadStart(CheatLoop));
+			cheatThread.Start();
+		}
+
+		public static void CheatLoop()
+		{
+			int i = 0;
+			while (true)
+			{
+				// do cheaty stuff
+				Console.WriteLine(i++);
+				if (i > 10) return;
+			}
+		}
+	}
+}
