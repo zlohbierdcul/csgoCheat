86d8836410717ba47a6e277745c746258e80019d
zlohbierdcul <82916072+zlohbierdcul@users.noreply.github.com>
2023-03-05 19:43:33 +0100

    initial commit

---
+++ FirstCheat/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace FirstCheat
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            VAMemory vaMemory = new VAMemory("minecraft");
+
+            var client = vaMemory.GetModuleBaseAddress("client.dll");
+            var engine = vaMemory.GetModuleBaseAddress("engine.dll");
+
+        }
+
+
+    }
+
+
+}
